23cf5a34b13987b9e641e2f04c257a6e59fcc4dc
Adel <adelineeminian13@gmail.com>
2024-01-26 12:10:53 +0400

    add

--- trim.c
+++ trim.c
@@ -71,3 +71,172 @@ int main()
     char set[] = "ab";
     printf("%s", ft_strtrim(str, set));
 }
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+static char    *str_new(size_t n)
+{
+    char    *str;
+
+    str = (char *)malloc(sizeof(char) * (n + 1));
+    if (!str)
+        return (NULL);
+    return (str);
+} 
+int    check(char *str,unsigned char c)
+{
+    while (*str)
+    {
+        if (*str == c)
+            return (1);
+        str++;
+    }
+    return (0);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    size_t    size;
+    size_t    len;
+    size_t  start;
+    size_t end;
+   char *ss;
+    char *ptr;
+    
+    ptr = s1;
+    ss = set;
+    size = strlen(s1);
+    if (!s1 || !set)
+        return (NULL);
+    while (*s1 && *set)
+    {
+        if (check(set, *s1) && set)
+        {
+            *set++;
+            *s1++;
+                set = ss;
+        }
+        else
+        {
+        start = &s1;
+        break;
+        }
+    }
+    set = ss;
+    ptr += size - 1;
+    while (ptr)
+    {
+        if (check(set, *ptr))
+            {
+              *set++;
+              *ptr--;
+                  set = ss;
+            }
+            else{
+            end = &s1;
+            break;
+            }
+    }
+    len = end - start;
+    return (str_new(len));
+}
+int main()
+{
+    char    str[] = "cajbTRIMab";
+    char set[] = "ab";
+    printf("%s", ft_strtrim(str, set));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+static char    *str_new(size_t n)
+{
+    char    *str;
+
+    str = (char *)malloc(sizeof(char) * (n + 1));
+    if (!str)
+        return (NULL);
+    return (str);
+}
+int    check(char *str,unsigned char c)
+{
+    while (*str)
+    {
+        if (*str == c)
+            return (1);
+        str++;
+    }
+    return (0);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    size_t    size;
+    size_t    len;
+    size_t  start;
+    size_t end;
+   char *ss;
+    char *ptr;
+
+    ptr = s1;
+    ss = set;
+    size = strlen(s1);
+    if (!s1 || !set)
+        return (NULL);
+    while (*s1 && *set)
+    {
+        if (check(set, *s1) && set)
+        {
+            *set++;
+            *s1++;
+                set = ss;
+        }
+        else
+        {
+        start = &s1;
+        break;
+        }
+    }
+    set = ss;
+    ptr += size - 1;
+    while (ptr)
+    {
+        if (check(set, *ptr))
+            {
+              *set++;
+              *ptr--;
+                  set = ss;
+            }
+            else{
+            end = &s1;
+            break;
+            }
+    }
+    len = end - start;
+    return (str_new(len));
+}
+int main()
+{
+    char    str[] = "cajbTRIMab";
+    char set[] = "ab";
+    printf("%s", ft_strtrim(str, set));
+}
